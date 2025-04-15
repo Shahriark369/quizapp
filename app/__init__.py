5a35861834f78717d9ff4d71ed973d3aff8de356
Shahriar Kabir <shahriar369kabir@gmail.com>
2025-04-15 12:42:59 +0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quiz.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+from app import routes
 
